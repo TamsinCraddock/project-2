6b61d8626672abae475cb6424b8ffc5779437f70
TamsinCraddock <tamsincraddock@gmail.com>
2022-06-12 22:17:42 +0000

    adding next button changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,56 @@
+const answers = ["a","b","c","d"]
+const questions = [
+    {
+        question: 'what is answer 1',
+        answer: 0
+    },
+    {
+        question: 'what is answer 2',
+        answer: 2
+    },
+    {
+        question: 'what is answer 3',
+        answer: 1
+    },
+    {
+        question: 'what is answer 4',
+        answer: 0
+    }
+];
+const gameState = {
+    currentQuestion: 0,
+    currentScore: 0,
+    questionOutput : null,
+}
+
+
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event lsteners to them
 
 document.addEventListener("DOMContentLoaded", function(){
-    let buttons = document.getElementsByTagName("button");
+    console.log("loaded");
+    const buttons = document.getElementsByClassName("answer-button");
+    gameState.questionOutput = document.getElementById("question");
+    const nextButton = document.getElementById("next-button");
+
+    nextButton.addEventListener("click", function(){
+        gameState.currentQuestion++;
+        displayQuestion();
+    });
+
+
 
     for (let button of buttons) {
         button.addEventListener("click", function(){
-            if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked submit!");
-            } else {
-                let gameType = this.getAttribute("data-type");
-                alert(`You clicked $(gameType)`);
-            }
+            const answer = this.getAttribute("data-value");
+            alert(`You clicked ${answer}`);
         })
     }
+    runGame()
 })
 
 function runGame() {
-
+    displayQuestion();
 }
 
 function checkAnswer() {
@@ -37,5 +70,7 @@ function incrementWrongAnswer() {
 }
 
 function displayQuestion() {
-    
+    console.log("setting question");
+    gameState.questionOutput.textContent = questions[gameState.currentQuestion].question
+
 }
